989a5e857ea8af34a3567337a87a21a9d56ede8d
Matheus7k <06matheus.oliveira@gmail.com>
2023-03-05 22:55:56 -0300

    Add project files.

---
+++ alcool_gasolina/Program.cs
@@ -0,0 +1,22 @@
+﻿internal class Program
+{
+    static double CalculaPorcentagem(double precoGasolina, double precoAlcool)
+    {
+        return (precoAlcool * 100) / precoGasolina;
+    }
+    private static void Main(string[] args)
+    {
+        double pA, pG, p;
+
+        Console.WriteLine("Informe o preço do alcool: ");
+        pA = double.Parse(Console.ReadLine());
+        Console.WriteLine("Informe o preço da gasolina: ");
+        pG = double.Parse(Console.ReadLine());
+
+        p = CalculaPorcentagem(pA, pG);
+
+        if (p >= 72) Console.WriteLine("Gasolina é melhor!");
+        else Console.WriteLine("Alcool é melhor");
+
+    }
+}
